8deea8b8016d182ec4e01c5f15e2cd3739b7af62
Aman Sagar <amansagar102005@gmail.com>
2026-01-24 22:41:38 +0530

    more something

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,13 @@
 const express=require("express");
 const router=express.Router();
+const {registerdUser,loginUser,logOut}=require("../controllers/authController");
 
 router.get("/",function(req,res){
     res.send("hey");
-})
+});
+
+router.post("/register",registerdUser);
+
+router.post("/login",loginUser);
 
 module.exports=router;
